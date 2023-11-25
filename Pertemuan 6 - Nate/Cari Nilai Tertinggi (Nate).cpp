ab57517d27cd7da9ce421f678b82ad08f7d524bb
Nate <127719222+KidmanNPC@users.noreply.github.com>
2023-11-25 13:31:04 +0700

    Update Cari Nilai Tertinggi (Nate).cpp

--- Pertemuan 6 - Nate/Cari Nilai Tertinggi (Nate).cpp
+++ Pertemuan 6 - Nate/Cari Nilai Tertinggi (Nate).cpp
@@ -7,32 +7,26 @@ int main() {
     cout << "=======================================" << endl;
     cout << endl;
 
-    int input[360], arr_count, i, max_num; // Deklarasi 4 Variabel = input[360], arr_count, i, max_num
-                                           // input[360] = buat nampungin semua angka. Soalnya di set ama angka 360, jadi itu jumlah maksimum element array nya.
-                                           // arr_count = buat tempat input jumlah nilai angka yang mau dimasukkin tapi nga perlu input 360 angka sekaligus.
-
-    cout << "Input jumlah element Array: ";
-    cin >> arr_count;
-
-    cout << "Input "<< arr_count << " angka: " ;
-    cout << endl;
-
-  // setiap angka yang diinput disimpan ke dalam array
-    for(i = 0; i < arr_count; i++){
-    cin >> input[i];
+    cout << "Nilai-Nilai: " << endl; // Nampilin semua nilai dari array element
+    int sumArr[5] = {30, 60, 180, 120, 150};
+    for (int i = 0; i < 5; ++i) {
+        cout << "Nilai " << i + 1 << ": " << sumArr[i] << endl;
     }
 
-    cout << endl;
+    int nilTinggi = sumArr[0];
+    int index = 0;
 
-    max_num = input[0];
-  // proses mencari nilai terbesar
-    for(i = 0; i < arr_count; i++){
-    if(input[i] > max_num){
-        max_num = input[i];
+    for (int i = 1; i < 5; ++i) {     // Nyari nilai tertinggi ama indexnya
+        if (sumArr[i] > nilTinggi) {
+            nilTinggi = sumArr[i];
+            index = i;
         }
     }
 
-    cout  << "Angka paling terbesar adalah: " << max_num; // hasil angka nilai terbesar dari semua element array
     cout << endl;
-        return 0;
+
+    cout << "Nilai paling tertinggi: " << nilTinggi << endl;     // Menampilkan hasil nilai tertinggi dan index
+    cout << "Index yang nilainya tertinggi: " << index << endl;
+
+    return 0;
 }
